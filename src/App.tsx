1f3e7dd67d679b65bf276ad81bd076a2a71daa7c
dytra <vancielnugraha@gmail.com>
2023-12-16 19:38:10 +0700

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import { format, addDays, isAfter } from 'date-fns';
+import React, { useState, useEffect } from "react";
+import { format, addDays, isAfter } from "date-fns";
+import useLocalStorage from "react-storage-helper";
+import { Button } from "./components/ui/button";
+import { ThemeProvider } from "./components/theme-provider";
+import MainApp from "./components/MainApp";
 
+type WeekType = "technical" | "marketing";
 function App() {
-  const [weekType, setWeekType] = useState(localStorage.getItem('weekType') || 'technical');
+  const [weekType, setWeekType] = useState(
+    localStorage.getItem("weekType") || "technical"
+  );
   const [startDate, setStartDate] = useState(new Date());
   const endDate = addDays(new Date(startDate), 7);
+  const [currentWeekType, setCurrentWeekType] =
+    useLocalStorage<WeekType>("currentWeekType");
+  console.log("cur bro ", currentWeekType);
 
-  const handleButtonClick = (type) => {
+  const handleButtonClick = (type: string) => {
     // if (weekType !== type) {
-      setWeekType(type);
-      setStartDate(new Date());
-      localStorage.setItem('weekType', type);
-      localStorage.setItem('startDate', new Date().toISOString());
+    setWeekType(type);
+    setStartDate(new Date());
+    localStorage.setItem("weekType", type);
+    localStorage.setItem("startDate", new Date().toISOString());
     // }
   };
 
-  const isTechnicalWeek = weekType === 'technical';
-  const currentDateString = format(new Date(), 'MMMM d, yyyy');
+  // const isTechnicalWeek = weekType === "technical";
+  const currentDateString = format(new Date(), "MMMM d, yyyy");
+
+  useEffect(() => {
+    if (!currentWeekType) setCurrentWeekType("technical");
+  }, []);
 
   return (
-    <div className="App">
-      <h1>Business Week Type</h1>
-      <p>Current Week: {weekType.charAt(0).toUpperCase() + weekType.slice(1)}</p>
-      <p>Current Date: {currentDateString}</p>
-      <p>End Date: {format(endDate, 'MMMM d, yyyy')}</p>
-      <button onClick={() => handleButtonClick('technical')}>Technical Week</button>
-      <button onClick={() => handleButtonClick('marketing')}>Marketing Week</button>
-      {isAfter(new Date(), endDate) && (
-        <p>It's time to switch to a {weekType === 'technical' ? 'marketing' : 'technical'} week!</p>
-      )}
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="App">
+        <MainApp />
+        {/* <div className="container w-full flex flex-col">
+          <h1>Business Week Type</h1>
+
+          <p>
+            Current Week: {weekType.charAt(0).toUpperCase() + weekType.slice(1)}
+          </p>
+          <p>Current Date: {currentDateString}</p>
+          <p>End Date: {format(endDate, "MMMM d, yyyy")}</p>
+          <Button onClick={() => handleButtonClick("technical")}>
+            Technical Week
+          </Button>
+          <button onClick={() => handleButtonClick("marketing")}>
+            Marketing Week
+          </button>
+          {isAfter(new Date(), endDate) && (
+            <p>
+              It's time to switch to a{" "}
+              {weekType === "technical" ? "marketing" : "technical"} week!
+            </p>
+          )}
+        </div> */}
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/MainApp.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const MainApp = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+      <span
+        className="relative flex shrink-0 overflow-hidden rounded-full w-24 h-24 mb-8"
+        // src="/placeholder.svg?height=100&amp;width=100"
+      ></span>
+      <h1 className="text-4xl font-bold text-center mb-4">What Week Is This?</h1>
+      <div
+        className="rounded-lg border bg-card text-card-foreground shadow-sm mb-8 w-full max-w-md mx-auto"
+        data-v0-t="card"
+      >
+        <div className="flex-col space-y-1.5 p-6 flex items-center justify-between">
+          <h3 className="tracking-tight text-lg font-semibold">Current Week</h3>
+          <div
+            className="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-sm"
+            color="blue"
+          >
+            Technical Week
+          </div>
+        </div>
+        <div className="p-6 text-center">
+          <p className="text-sm text-gray-500">
+            This week is a technical week. Focus on enhancing your technical
+            skills.
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-4">
+        <a className="text-blue-500 hover:underline" href="#" rel="ugc">
+          Previous Weeks
+        </a>
+        <button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
+          Next Week
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MainApp;
