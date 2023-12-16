d9169495208d1d6e446190afb560fccb57f80394
dytra <vancielnugraha@gmail.com>
2023-12-16 23:33:21 +0700

    add dynamice weekTypeString var

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,12 +1,22 @@
+import { WeekType } from "@/types";
+import useLocalStorage from "@/vendor/react-storage-helper/useLocalStorage";
+// import useLocalStorage from "react-storage-helper";
 
 const MainApp = () => {
+  const [currentWeekType, setCurrentWeekType] =
+    useLocalStorage<WeekType>("currentWeekType","marketing");
+  const weekTypeString =
+    currentWeekType === "marketing" ? "Marketing" : "Technical";
+  // console.log("currentWeekTypexu", currentWeekType)
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
       <span
         className="relative flex shrink-0 overflow-hidden rounded-full w-24 h-24 mb-8"
         // src="/placeholder.svg?height=100&amp;width=100"
       ></span>
-      <h1 className="text-4xl font-bold text-center mb-4">What Week Is This?</h1>
+      <h1 className="text-4xl font-bold text-center mb-4">
+        What Week Is This?
+      </h1>
       <div
         className="rounded-lg border bg-card text-card-foreground shadow-sm mb-8 w-full max-w-md mx-auto"
         data-v0-t="card"
@@ -17,7 +27,7 @@ const MainApp = () => {
             className="inline-flex items-center rounded-full border px-2.5 py-0.5 w-fit font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-sm"
             color="blue"
           >
-            Technical Week
+            {weekTypeString} Week
           </div>
         </div>
         <div className="p-6 text-center">
