5272fb807527bb9427c7e10ab9ce91a71cff2ac2
dytra <vancielnugraha@gmail.com>
2023-12-16 21:20:44 +0700

    remove React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { format, addDays, isAfter } from "date-fns";
 import useLocalStorage from "react-storage-helper";
 import { Button } from "./components/ui/button";
