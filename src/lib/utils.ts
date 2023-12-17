a07f658a8caf8cc46a9f0bc95f9df2179c4ecb05
dytra <vancielnugraha@gmail.com>
2023-12-17 09:26:21 +0700

    add getCurrentWeekType func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { WeekType } from "@/types";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -11,4 +12,15 @@ export function capFirst(str: string) {
     return str;
   }
   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+export function getCurrentWeekType(initialDate: Date, initialWeekType: WeekType, currentDate: Date): WeekType {
+  const timeDiff = currentDate.getTime() - initialDate.getTime();
+  const weeksPassed = Math.floor(timeDiff / (7 * 24 * 60 * 60 * 1000));
+
+  const cycle = ["technical", "marketing"];
+  const currentWeekIndex = weeksPassed % cycle.length;
+
+  return cycle[currentWeekIndex] as WeekType;
 }
