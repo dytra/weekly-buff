7475a02b3fe9f1023ca4376d26c2e03964710dea
dytra <vancielnugraha@gmail.com>
2023-12-17 08:43:45 +0700

    add capFirst utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function capFirst(str: string) {
+  if (typeof str !== "string" || str.length === 0) {
+    console.error("Input should be a non-empty string");
+    return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
